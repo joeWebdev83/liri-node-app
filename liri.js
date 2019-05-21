60b7249e57edb2ab03c9a41b84cdfdf20781be45
joel petion <joelpetion@yahoo.com>
2019-05-20 23:40:55 -0400

    done

--- liri.js
+++ liri.js
@@ -7,63 +7,64 @@ const fs = require("fs");
 const Spotify = require("node-spotify-api");
 
 //displays input and controls spacing
-const operation = process.argv[2];
-const info = process.argv.slice(3).join(" ");
+var operation = process.argv[2];
+var info = process.argv.slice(3).join(" ");
 
 //App Logic
-switch (operation) {
- case "concert-this": {
-   concerts();
-   break;
- }
- case "movie-this": {
-   movies();
-   break;
- }
+function initialize(){
+  switch (operation) {
+    case "concert-this": {
+      concerts();
+      break;
+    }
+    case "movie-this": {
+      movies();
+      break;
+    }
+   
+    case "spotify-this-song": {
+      songs();
+      break;
+    }
+   
+    case "do-what-it-says": {
+      whatever();
+      break;
+    }
+    default: {
+      console.log("I don't understand what you want");
+    }
+   }
 
- case "spotify-this-song": {
-   songs();
-   break;
- }
-
- case "do-what-it-says": {
-   whatever();
-   break;
- }
- default: {
-   console.log("I don't understand what you want");
- }
 }
 
+
+initialize();
 //Functions for each case
 function concerts() {
- const concertApi =
-   "https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp";
-
- axios.get(concertApi).then(function(response) {
-   const data= response.data[0];
-   const concertData=[
-     
-    //"*******************************************",
-     
-     "Artist: " + info,
-     "Venue: " + data.venue.name + ", " + data.venue.region,
-     "Location: " + data.venue.city + ", " + data.venue.region,
-     "Date: " + moment(data.datetime).format("l"),
-     
-
-
-   ]
-   console.log(concertData);
-  })
-  .catch(function(error){
-    console.log(error);
-  });
-
+  const concertApi =
+    "https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp";
  
-
-   
-
+  axios.get(concertApi).then(function(response) {
+    const data= response.data[0];
+    const concertData=[
+      
+     
+      
+      "Artist: " + info,
+      "Venue: " + data.venue.name + ", " + data.venue.region,
+      "Location: " + data.venue.city + ", " + data.venue.region,
+      "Date: " + moment(data.datetime).format("l"),
+      
+ 
+ 
+    ]
+    console.log(concertData);
+   })
+   .catch(function(error){
+     console.log(error);
+   });
+  } 
 
 function songs(){
  const spotify = new Spotify({
@@ -94,14 +95,13 @@ function songs(){
 
      }
 
-
-     function movies() {
+function movies() {
  const movieApi =
    "http://www.omdbapi.com/?t=" + info + "&y=&plot=short&apikey=trilogy";
 
  axios.get(movieApi).then(function(response) {
    const film = response.data;
-   // console.log(response.data);
+    
 
    console.log("Movie Tittle: "+ film.Title);
    console.log("Year: " + film.Year);
@@ -112,14 +112,17 @@ function songs(){
    console.log("Plot: " + film.Plot);
    console.log("Actors: " + film.Actors);
  });
- 
- 
- function whatever(){
+}
+
+function whatever(){
   fs.readFile('random.txt', "UTF8", function(error, data){
     var txt = data.split(',');
+     operation= txt[0]
+    
+    info= txt[1]
+    
+     initialize();
  
-    spotifyThisSong(txt[1]);
+   
   });
-}
-}
-}
+ }
